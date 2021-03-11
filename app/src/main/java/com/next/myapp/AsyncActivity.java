4c47169e59b4564480f9991bf46fd804ef6e4253
abdul <abdul.tanveer@gmail.com>
2021-03-11 20:11:32 +0530

    asyntask part3

--- app/src/main/java/com/next/myapp/AsyncActivity.java
+++ app/src/main/java/com/next/myapp/AsyncActivity.java
@@ -4,19 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 public class AsyncActivity extends AppCompatActivity {
     ProgressBar hProgressBar;
+    ImageView imageView;
+    String url;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_async);
         hProgressBar = findViewById(R.id.progressBar);
+        imageView = findViewById(R.id.imageView2);
+        url = "https://www.wishgoodmorning.org/wp-content/uploads/2016/03/Awesome-Morning-Flowers-wg01004-600x449.jpg";
     }
 
+
     public void download(View view) {
-        DownloadTask downloadTask = new DownloadTask(hProgressBar);
-        downloadTask.execute("http://urlforImagetobedownloaded.com");
+        DownloadTask downloadTask = new DownloadTask(hProgressBar,imageView);
+        downloadTask.execute(url);
     }
 }
--- app/src/main/java/com/next/myapp/DownloadTask.java
+++ app/src/main/java/com/next/myapp/DownloadTask.java
@@ -1,17 +1,25 @@
 package com.next.myapp;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+
+import java.io.IOException;
+import java.net.URL;
+
 //inputtype = url= string, Integer = progress type = percentage, Bitmap = result type
 public class DownloadTask extends AsyncTask<String,Integer, Bitmap> {
     public static String TAG = DownloadTask.class.getSimpleName();
     ProgressBar dtProgressBar;
+    ImageView dtImageView;
 
-    public DownloadTask(ProgressBar hProgressBar) {
+    public DownloadTask(ProgressBar hProgressBar, ImageView imageView) {
         dtProgressBar = hProgressBar;
+        dtImageView = imageView;
     }
 
     @Override
@@ -30,16 +38,23 @@ public class DownloadTask extends AsyncTask<String,Integer, Bitmap> {
     @Override
     protected Bitmap doInBackground(String... strings) {
         Log.i(TAG,"downloading image from the url="+ strings[0]);
+        Bitmap image = null;
         //mimic download of 5% every 200 ms
-        try {
+      /*  try {
         for(int i=1; i<21; i++) {
             publishProgress(i * 5); //background student telling the scores to teacher
             Thread.sleep(100);
         }
             } catch (InterruptedException e) {
                 e.printStackTrace();
-            }
-        return null;
+            }*/
+        try {
+            URL url = new URL(strings[0]);
+             image = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+        } catch(IOException e) {
+            System.out.println(e);
+        }
+        return image;
     }
 
     @Override
@@ -51,5 +66,6 @@ public class DownloadTask extends AsyncTask<String,Integer, Bitmap> {
     @Override
     protected void onPostExecute(Bitmap bitmap) {
         super.onPostExecute(bitmap);
+        dtImageView.setImageBitmap(bitmap);
     }
 }
