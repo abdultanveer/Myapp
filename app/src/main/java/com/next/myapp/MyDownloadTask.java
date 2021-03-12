7784dccc098cbf8c45c4fc77a7c2efa56949a63f
abdul <abdul.tanveer@gmail.com>
2021-03-12 11:37:52 +0530

    asyntask part-B

--- app/src/main/java/com/next/myapp/MyDownloadTask.java
+++ app/src/main/java/com/next/myapp/MyDownloadTask.java
@@ -3,6 +3,7 @@ package com.next.myapp;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.view.View;
 import android.widget.ProgressBar;
 
 /**
@@ -11,15 +12,24 @@ import android.widget.ProgressBar;
 //params -- input type, progress type and result type
 public class MyDownloadTask  extends AsyncTask<String,Integer, Bitmap> {
     public static String TAG = MyDownloadTask.class.getSimpleName();
-    
+    ProgressBar mProgressBar;
 
     public MyDownloadTask(ProgressBar downloadProgressBar) {
+        mProgressBar = downloadProgressBar;
+    }
+
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        mProgressBar.setVisibility(View.VISIBLE);
     }
 
     //doInBackground executes in a seperate thread
     @Override
     protected Bitmap doInBackground(String... strings) {
         Log.i(TAG,"url is"+strings[0]);
+        mProgressBar.setProgress(50);
         return null;
     }
 }
